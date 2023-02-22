4deb8030d86d7bacd56e129fd6744700753c6216
神马都要问 <shenmadouyaowen@gmail.com>
2023-02-22 16:11:58 +0800

    Update wxchatbot.py
    使用redis 存储答案

--- wxchatbot.py
+++ wxchatbot.py
@@ -2,7 +2,19 @@ import werobot
 from revChatGPT.V1 import Chatbot
 import multiprocessing
 import time
+import redis
+redis_url="127.0.0.1"
+redis_pwd=""
 
+def redis_conn():
+    try:
+        pool = redis.ConnectionPool(host=redis_url, port=6379, decode_responses=True, socket_connect_timeout=5, password=redis_pwd)
+        r = redis.Redis(connection_pool=pool)
+        r.get('conn_test')
+        print('✅redis连接成功')
+        return r
+    except:
+        print("⚠️redis连接异常")
 
 def timeout_function(p, timeout,session):
     # 等待子进程完成或超时
@@ -13,13 +25,14 @@ def timeout_function(p, timeout,session):
     else:
         # 子进程完成，返回子进程结果
         #session['content']=p.exitcode
+        print(p.exitcode)
         print("进程超时")
-        #return session['content']
+        return "进程超时"
 
 
 #此处填微信公众号token ,可以自定义
 robot = werobot.WeRoBot(token='tokenhere')
-#自定义端口
+#自定义本地程序端口
 robot.config.update(
     HOST='0.0.0.0',
     PORT=8108
@@ -28,12 +41,11 @@ robot.config.update(
 chatbot = Chatbot(config={
     "access_token":"填入获取到的<access_token>"
     })  
-
-
 #revChatGPT 根据问题获取chatgpt答案
 #text 问题
-#session 把获取到的答案放入session
-def getchatpgt(text,session):
+#把内容放入redis
+
+def getchatpgt(text):
     prev_text = ""
     for data in chatbot.ask(
     text,
@@ -41,31 +53,32 @@ def getchatpgt(text,session):
         message = data["message"][len(prev_text) :]
         #print(message, end="", flush=True)
         prev_text = data["message"]
-    #print(prev_text)
-    session['content']=prev_text
+    #print("结果:"+prev_text)
+    r.set('content',prev_text)
     return prev_text
 
 
 @robot.text
 def first(message, session):
-	#检查之前是否提问过
-    if "chatgpt" in message.content and  session['content'] != None:
-        content=session['content']
-        session['content']=None
+    #检查之前是否提问过
+    if "chatgpt" in message.content and  r.get('content') != "":
+        content=r.get('content')
+        r.set('content',"")
         return content
-	#发送chatgpt 直接返回
-    elif "chatgpt" in message.content and session['content']==None:
+    #发送chatgpt 直接返回
+    elif "chatgpt" in message.content and r.get('content')=="":
         return "你好！我就是 ChatGPT，一个由 OpenAI 训练的大型语言模型。有什么我可以帮助你的吗？"
-		
-    if __name__ == '__main__':
-        p = multiprocessing.Process(target=getchatpgt,args=(message.content,session))
-        p.start()
-        result = timeout_function(p, 4,session)  # 设置函数执行的时间限制为 4秒钟
-        if 'success' in result:
-           return '超时了,请等待15s左右输入chatgpt 获取问题答案' 
+
+    p = multiprocessing.Process(target=getchatpgt,args=(message.content,))
+    p.start()
+    result = timeout_function(p, 4,session)  # 设置函数执行的时间限制为 4秒钟
+    if 'success' in result:
+        return '我正在思考,请等待15s左右输入chatgpt 获取问题答案'
+    
         #session['content']=result
     #print('timeout_function结果：', result)
     #print('结果：', session['content'])
-    return session['content']
-
-robot.run()
+    return r.get('content')
+if __name__ == '__main__':
+    r = redis_conn()
+    robot.run()
